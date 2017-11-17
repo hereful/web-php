c30b69deebb028c773365f853caeae4da649e34c
Alex <notlose@hotmail.com>
2017-11-17 17:44:44 +0800

    debug

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,96 +1,12 @@
-<!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
 
-        <title>Laravel</title>
+$command = '/check 700';
+$commands = explode(' ', $command);
+$returnMessage = '';
+if (@$commands[0] == '/check' && @$commands[1]) {
+    $stockCode = str_pad($commands[1], 5, '0', STR_PAD_LEFT);
+    $returnMessage = @file_get_contents('http://qt.gtimg.cn/?q=s_hk' . $stockCode);
+}
+echo $returnMessage;
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            
-
-            <div class="content">
-                <div class="title m-b-md" id="demo">
-                    Nothing
-                </div>
-
-                
-            </div>
-        </div>
-        <script>
-            var x = document.getElementById("demo");
-            function getLocation() {
-                if (navigator.geolocation) {
-                    navigator.geolocation.getCurrentPosition(showPosition);
-                } else {
-                    x.innerHTML = "Geolocation is not supported by this browser.";
-                }
-            }
-            function showPosition(position) {
-                x.innerHTML = "Latitude: " + position.coords.latitude + 
-                "<br>Longitude: " + position.coords.longitude; 
-            }
-            
-            getLocation();
-        </script>
-    </body>
-</html>
+?>
