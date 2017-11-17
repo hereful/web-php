9d6455ec9a23af29acb08969dd2c2bda10e28def
Alex <notlose@hotmail.com>
2017-11-17 17:30:51 +0800

    debug

--- app/Http/Controllers/Api/CheckinController.php
+++ app/Http/Controllers/Api/CheckinController.php
@@ -51,7 +51,7 @@ class CheckinController extends \App\Http\Controllers\Controller {
         $command = $request->input('message.text');
         $commands = explode(' ',$command);
         $returnMessage = '';
-        if(@$commands[0]=='check' && @$commands[1]){
+        if(@$commands[0]=='/check' && @$commands[1]){
             $stockCode = str_pad($commands[1], 5, '0', STR_PAD_LEFT);
             $returnMessage = @file_get_contents('http://qt.gtimg.cn/?q=s_hk'.$stockCode);
         }
